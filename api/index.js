38ebec0f257be98efcd316084f363ea8c5ffa252
ahmedbjd06 <a.boudjediene@esi-sba.dz>
2024-03-22 15:11:13 +0100

    connected to mongodb

--- api/index.js
+++ api/index.js
@@ -1,8 +1,21 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("mongodb connected successfuly");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 const port = 4000;
 
 app.listen(port, () => {
-    console.log(`server listening on port ${port} ...`)
-})
+  console.log(`server listening on port ${port} ...`);
+});
