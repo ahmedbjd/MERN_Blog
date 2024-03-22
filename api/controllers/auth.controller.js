1db33f4214e32f95daec17349b43cc10a405f3f5
ahmedbjd06 <a.boudjediene@esi-sba.dz>
2024-03-22 17:40:45 +0100

    add hased password

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,4 +1,5 @@
 import User from "../models/user.model.js";
+import bcryptjs from "bcryptjs";
 
 export const signup = async (req, res) => {
   const { username, email, password } = req.body;
@@ -13,16 +14,18 @@ export const signup = async (req, res) => {
     res.status(400).json({ msg: "All fields are required" });
   }
 
+  const hashedPassword = bcryptjs.hashSync(password, 10);
+
   const newUser = new User({
     username,
     email,
-    password,
+    password: hashedPassword,
   });
 
   try {
     await newUser.save();
     res.json({ msg: "Signup successful" });
   } catch (error) {
-    res.status(500).json({ msg: "error user signup" });
+    res.status(500).json({ msg: error.message });
   }
 };
