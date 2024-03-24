109f3154696de9d3f7660c820c525bb5df719974
ahmedbjd06 <a.boudjediene@esi-sba.dz>
2024-03-24 01:25:02 +0100

    add google auth (i have an error)

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -42,10 +42,7 @@ export const signin = async (req, res, next) => {
     if (!validUser) {
       return next(errorHandler(404, "user not found"));
     }
-    const validPassword = await bcryptjs.compareSync(
-      password,
-      validUser.password
-    );
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
     if (!validPassword) {
       return next(errorHandler(400, "password incorrect"));
     }
@@ -58,6 +55,43 @@ export const signin = async (req, res, next) => {
       .status(200)
       .cookie("access_token", token, { httpOnly: true })
       .json({ msg: "signin successful", data: rest });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const google = async (req, res, next) => {
+  const { name, email, googlePhotoUrl } = req.body;
+  try {
+    const user = await User.findOne({ email });
+    if (user) {
+      const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+      const { password, ...rest } = user._doc;
+      res
+        .status(200)
+        .cookie("access_token", token, { httpOnly: true })
+        .json({ msg: "signin successful", data: rest });
+    } else {
+      const generatedPassword =
+        Math.random().toString(36).slice(-8) +
+        Math.random().toString(36).slice(-8);
+      const hashedPassword = bcryptjs.hashSync(generatedPassword, 10);
+      const newUser = new User({
+        username:
+          name.toLowerCase().split(" ").join("") +
+          Math.random().toString(9).slice(-4),
+        email,
+        password: hashedPassword,
+        profilePicture: googlePhotoUrl,
+      });
+      await newUser.save();
+           const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+           const { password, ...rest } = user._doc;
+           res
+             .status(200)
+             .cookie("access_token", token, { httpOnly: true })
+             .json({ msg: "signin successful", data: rest });
+    }
   } catch (error) {
     next(error);
   }
